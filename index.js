6b9a830d6bd84019599c9c161ab628071d85811d
Mindaugas <strumke@yahoo.com>
2020-10-07 15:50:17 +0200

    res fixed

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/api/movies", async (req, res) => {
     const response = await axios.get(
       `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=2020&year=2020&with_genres=878`
     );
-    res.send(response.data.results);
+    res.json(response.data.results);
   } catch (err) {
     console.log("=========ERRORR===========", err);
     res.send({ message: "something went wrong" });
