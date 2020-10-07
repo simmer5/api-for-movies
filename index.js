a0cda0e9f59b56e3ce20514060e5d0ccbb47eccc
Mindaugas <strumke@yahoo.com>
2020-10-07 13:57:44 +0200

    fixed .env

--- index.js
+++ index.js
@@ -16,16 +16,6 @@ app.get("/api/movies", async (req, res) => {
     res.send({ message: "something went wrong" });
   }
 });
-app.get("/api/movies/", async (req, res) => {
-  try {
-    const response = await axios.get(
-      `https://api.themoviedb.org/3/discover/movie?api_key=8cd009597749ca7c4f139d1147b23f9e&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=2020&year=2020&with_genres=878`
-    );
-    res.send(response.data.results);
-  } catch (err) {
-    res.send({ message: "something went wrong" });
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
